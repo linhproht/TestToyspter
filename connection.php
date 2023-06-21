3e75f50936ca4db951dae5507a5171149fcd72f3
linhproht <vnguyenduylinh@gmail.com>
2023-06-21 13:08:01 +0700

    push

--- connection.php
+++ connection.php
@@ -1,4 +1,4 @@
 <?php
-$conn = mysqli_connect('localhost', 'root', '', 'online_shopping')
+$conn = mysqli_connect('us-cdbr-east-06.cleardb.net', 'bd731c0b030b3b', '2dcd80d1', 'Toystore')
     or die("Can't coonec to database" . mysqli_connect_error());
 ?>
