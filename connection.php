c09f810c95a766d8ae4bfef32a41e521dc123ead
linhproht <vnguyenduylinh@gmail.com>
2023-06-21 14:51:13 +0700

    .

--- connection.php
+++ connection.php
@@ -1,4 +1,4 @@
 <?php
-$conn = mysqli_connect('us-cdbr-east-06.cleardb.net', 'bd731c0b030b3b', '2dcd80d1', 'Toystore')
+$conn = mysqli_connect('us-cdbr-east-06.cleardb.net', 'b839c1dd9c98e9', '5245f41f', 'heroku_90d126786a35a20')
     or die("Can't coonec to database" . mysqli_connect_error());
 ?>
