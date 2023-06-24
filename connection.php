234cae13cd664252fc694bcdec8b8ff024e722d1
linhproht <vnguyenduylinh@gmail.com>
2023-06-24 15:10:57 +0700

    .

--- connection.php
+++ connection.php
@@ -1,4 +1,4 @@
 <?php
-$conn = mysqli_connect('us-cdbr-east-06.cleardb.net', 'b35014a7ffaa7e', '733a0836', 'heroku_f9d8344f072e2fa')
+$conn = mysqli_connect('us-cdbr-east-06.cleardb.net', 'bf784ef8968679', 'efd08b06', 'heroku_9ebf4122bf166a8')
     or die("Can't coonec to database" . mysqli_connect_error());
 ?>
